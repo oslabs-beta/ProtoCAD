5c9ea38416c740a230065b2161e45717f46f463e
Suramya <suramyasekhri@gmail.com>
2019-10-10 19:54:48 -0700

    nothing changed

--- client/components/SchemaBoard.tsx
+++ client/components/SchemaBoard.tsx
@@ -1,4 +1,9 @@
 import * as React from 'react';
+//import CodeMirror from 'codemirror';
+
+//let myCodeMirror = CodeMirror(document.body);
+
+
 
 export default props => {
    return <div id={'schemaBoard'}>Schema Board Content</div>

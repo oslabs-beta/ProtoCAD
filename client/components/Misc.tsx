3f26d9ae3489193d600bd41fbe45fa23799496e7
Benjamin L. Morrison <newben.hd@gmail.com>
2019-10-10 17:04:17 -0700

    connected backend and front-end

--- client/components/Misc.tsx
+++ client/components/Misc.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 
+declare const window: any;
+
+// const electron = window.require('electron');
+// const fs = electron.remote.require('fs');
+const ipcRenderer = window.ipcRenderer;
 
 export default props => {
 
   const components = useSelector(state => state.components.data);
 
   const onClick = () => {
-      console.log(components)
+      ipcRenderer.send('schema', components);
   };
+
+    ipcRenderer.on('schema', (e, data) => {
+        console.log(data);
+    });
+
+  React.useEffect(() => {
+  },[]);
 
    return <div id={'misc'}>
     <button onClick={onClick}>Export Schema</button>

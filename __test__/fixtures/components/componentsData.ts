a6f799adafab17f151c6fd8552fb5976dae58390
Benjamin L. Morrison <newben.hd@gmail.com>
2019-10-12 11:12:55 -0700

    setup testing and added unit tests for redux

---
+++ __test__/fixtures/components/componentsData.ts
@@ -0,0 +1,12 @@
+export const addComponent = {
+    name: 'C1',
+    attributes: { id: 'ID' },
+    children: []
+};
+
+export const componentsReducerData = {
+    name: 'C1',
+    attributes: {id: 'ID'},
+    children: []
+};
+
--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,24 @@ const {defaults} = require('jest-config');
 module.exports = {
   verbose: true,
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  setupFiles: [
+    "<rootDir>/__test__/setup/test-shim.js",
+    "<rootDir>/__test__/setup/test-setup.js",
+  ],
+  preset: "ts-jest/presets/js-with-ts",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tests/setup/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/tests/setup/styleMock.js"
+  },
+  moduleDirectories: [
+    "node_modules",
+    "dist"
+  ],
+  testMatch: [
+    "**/__test__/**/*.test.(ts|tsx|js)"
+  ],
+  collectCoverage: true
 };

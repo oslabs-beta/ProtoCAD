25466b552f0575975fa50ea1848019e5398a99e7
kassanj <kassandram022@gmail.com>
2019-10-25 15:22:29 -0700

    fix testing on enzyme for reducers and actions

--- client/reducer/currentComponentReducer.ts
+++ client/reducer/currentComponentReducer.ts
@@ -7,43 +7,7 @@ import {
 const defaultState: CurrentComponentStateInt = {
     error: null,
     loading: null,
-    data: {
-      name: 'School',
-      attributes: {
-        'id': 'ID',
-        'name': 'String',
-      },
-      parent: {},
-      children: [{
-        name: 'Student',
-        attributes: {
-          'id': 'ID',
-          'name': 'String',
-          'cohort': 'Int',
-        },
-        parent: {},
-        children: []
-      },
-      {
-        name: 'Fellow',
-        attributes: {
-          'id': 'ID',
-          'name': 'String',
-          'cohort': 'Int',
-        },
-        parent: {},
-        children: []
-      },
-      {
-        name: 'Instructor',
-        attributes: {
-          'id': 'ID',
-          'name': 'String',
-        },
-        parent: {},
-        children: []
-      }]
-    }
+    data: {}
 };
 
 export default (state: any = defaultState, action: any) => {

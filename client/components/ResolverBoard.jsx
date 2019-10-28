bd706e6887710187733ee654c0e6d79244f38536
Suramya <suramyasekhri@gmail.com>
2019-10-28 15:47:54 -0700

    added comments to component files

--- client/components/ResolverBoard.jsx
+++ client/components/ResolverBoard.jsx
@@ -1,26 +1,16 @@
 import * as React from 'react';
 import { Controlled as CodeMirror } from 'react-codemirror2'
-import {useDispatch, useSelector } from "react-redux";
 
+// renders resolver from parent component (Editor)
 export default props => {
-  // const [resolverCode, setCode] = React.useState(useSelector(state => state.resolver.data));
-  const dispatch = useDispatch();
-
-  // const {resolver, setResolver} = React.useContext(ResolveContext);
-
   const [option, setOptions] = React.useState({
     lineNumbers: true,
     lineWrapping: true,
   });
 
-  const onChange = (editor, data, value) => {
-
-  };
-
   const onBeforeChange = (editor, data, value) => {
-    // dispatch(setResolverCode(value));
     props.setResolver(value);
   };
 
-  return <CodeMirror value={props.resolver} onBeforeChange={onBeforeChange} onChange={onChange} options={option} />;
+  return <CodeMirror value={props.resolver} onBeforeChange={onBeforeChange} options={option} />;
 };

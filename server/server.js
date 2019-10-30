3bee080be378d42942e78967fe223c3f23de88d5
jasonlee1245 <jasonlee1245@gmail.com>
2019-10-30 14:11:41 -0700

    format of example data

--- server/server.js
+++ server/server.js
@@ -1,54 +1,19 @@
-const axios = require('axios');
 const { graphql, buildSchema } = require('graphql');
+const { schema, root, query } = require('./utils/launchSchema');
+//const { schema, root, query } = require('./utils/bookSchema');
 
 // runs whenever it receives messsage from electron
 // msg {array} params includes data for query, path, schema
 process.on('message', (msg) => {
-  //const schema =  buildSchema(`${msg[0]}`);
-  const query = msg[1]
+  // const schema =  buildSchema(`${msg[0]}`);
+  // const query = msg[1]
   // let path = `${msg[2]}/resolver.js`
   // const root = require(path)
-  const schema = buildSchema(`
-    type Launch {
-      id: ID,
-      launch_year: String,
-      mission_name: String,
-      rocket: Rocket,
-    }
-
-    type Rocket {
-      id: ID,
-      rocket_id: String,
-      rocket_name: String,
-      rocket_type: String,
-    }
-
-    type Query {
-      launch: Launch,
-      allLaunch: [Launch],
-      rocket: Rocket,
-    }`
-  )
-  const root = {
-    launch: () => 
-      {
-        return axios
-          .get('https://api.spacexdata.com/v3/launches/latest')
-          .then(res => res.data)
-      },
-    allLaunch: () =>
-      {
-        return axios
-          .get('https://api.spacexdata.com/v3/launches/')
-          .then(res => res.data)
-      }
-  }
-
-  // const query = '{launch {launch_year mission_name}}'
 
   // makes graphql query
   try {
     graphql(schema, query, root).then((response) => {
+      console.log(response);
       process.send(response);
     })
   }
